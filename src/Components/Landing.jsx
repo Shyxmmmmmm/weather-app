c27c03a1f7e49dd7d78758e70bca89aae82a0a51
shyxmmmmmm <shyamkumar2005464@gmail.com>
2026-04-17 13:11:17 +0530

    updated

--- src/Components/Landing.jsx
+++ src/Components/Landing.jsx
@@ -31,11 +31,11 @@ const Landing = () => {
             function (items) {
                 const tempC = (items.data.main.temp - 273.15).toFixed(1)
 
-              
 
-                settext(ip1.trim().charAt(0).toUpperCase()+ ip1.trim().slice(1))
 
-                
+                settext(ip1.trim().charAt(0).toUpperCase() + ip1.trim().slice(1))
+
+
                 settemp(tempC)
                 sethmdy(items.data.main.humidity)
                 setwspeed(items.data.wind.speed)
@@ -69,49 +69,40 @@ const Landing = () => {
                 setimg(weatherImages[iconCode])
             }
         ).catch(
-            ()=>{
+            () => {
                 alert(`City Not Found ❌`)
             }
         )
     }
     return (
-        <div className="min-h-screen bg-[#E3D8FF] flex flex-col items-center justify-center">
-            <h1 className='text-2xl  font-bold mb-10 '>Weather Reporting App</h1>
-            <div className="bg-[#4230BD] p-10 w-[80%] md:w-[50%] lg:w-[30%] rounded-2xl shadow-2xl">
-                <div className="flex gap-10">
-                    <input value={ip1} onChange={(e) => {
-                        setip1(e.target.value)
-                    }} type="text" placeholder="Search" className="pl-5 w-[75%] bg-white p-2 rounded-3xl" />
-                    <button onClick={funcadd} className="bg-white cursor-pointer p-2 w-10 rounded-full"><i class="fa-solid fa-magnifying-glass fa-xs " style={{ color: '#566566' }}></i></button>
+        <div className="bg-[#E3D8FF] min-h-screen flex flex-col justify-center items-center">
+            <h1 className="font-bold text-2xl">Weather Reporting App</h1>
+            <div className="mt-5 bg-[#4230BD] p-10 rounded-2xl md:w-[65%] w-[70%] lg:w-[30%]">
+                <div className="flex justify-between ">
+                    <input onChange={(e) => { setip1(e.target.value) }} value={ip1} type="text" placeholder="Search..." className="bg-white pl-4 p-2 w-[70%] rounded-full" />
+                    <button onClick={funcadd} className="bg-white p-2 rounded-full w-10 cursor-pointer"><i className="fa-solid fa-magnifying-glass fa-xs " style={{ color: '#566566' }}></i></button>
                 </div>
-                <div className='flex justify-center mt-10'>
-                    <img className='max-w-30' src={img} alt="" />
-
+                <div className='flex flex-col justify-center items-center mt-10'>
+                    <img src={img} alt="" className='max-w-30' />
+                    <p className='mt-3 font-semibold text-5xl text-white'>{temp && `${temp}°C`}</p>
+                    <h1 className='mt-2 font-bold text-3xl text-white'>{text}</h1>
                 </div>
-
-                <p className='text-white text-center mt-3 text-5xl font-semibold'>{temp && `${temp}°C`}</p>
-                <p className='text-white text-center mt-3 text-3xl font-semibold'>{text}</p>
-
-                <div className='flex mt-10'>
-
-
-                    <div className='flex-1 flex gap-2 items-center justify-center'>
-                        <i className="fa-solid fa-smog fa-xl text-white"></i>
+                <div className='flex mt-8 px-5 justify-between'>
+                    <div className='flex items-center gap-3'>
+                        <i className="fa-solid fa-smog fa-xl text-white" />
                         <div>
-                            <p className='text-white'>{hmdy && `${hmdy}%`} </p>
+                            <p className='text-center text-white'>{hmdy && `${hmdy}%`}</p>
                             <h1 className='text-white font-bold'>Humidity</h1>
                         </div>
                     </div>
 
-
-                    <div className='flex-1 flex gap-2 items-center justify-center'>
-                        <i className="fa-solid fa-wind fa-xl text-white"></i>
+                    <div className='flex items-center gap-3'>
+                        <i className="fa-solid fa-wind fa-xl text-white" />
                         <div>
-                            <p className='text-white'>{wspeed && `${wspeed}Km/h`} </p>
+                            <p className='text-center text-white'>{wspeed && `${wspeed} Km/h`}</p>
                             <h1 className='text-white font-bold'>Wind Speed</h1>
                         </div>
                     </div>
-
                 </div>
             </div>
         </div>
